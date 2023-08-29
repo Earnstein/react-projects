f9e647502ff7d1821bb38905f521e8052214f53c
Bakare Damilola <bdamilola00@gmail.com>
2023-08-29 02:10:25 +0100

    button image made dynamically

--- nike/src/components/Button.jsx
+++ nike/src/components/Button.jsx
@@ -4,9 +4,9 @@ const Button = ({label, iconURL}) => {
             gap-2 px-8 py-4 font-montserrat text-lg 
             leading-none bg-coral-red rounded-full border-coral-red">
         {label}
-        <img src={iconURL} alt="arrow right icon"
+        {iconURL && <img src={iconURL} alt="arrow right icon"
         className="ml-2 rounded-full w-5 h-5"
-        />
+        />}
     </button>
   )
 }
