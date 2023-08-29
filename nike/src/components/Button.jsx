e23b48b34db8a5ec61362818f20104c5f3cc2940
Bakare Damilola <bdamilola00@gmail.com>
2023-08-29 03:38:10 +0100

    refactored button classname

--- nike/src/components/Button.jsx
+++ nike/src/components/Button.jsx
@@ -1,14 +1,31 @@
-const Button = ({label, iconURL}) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  textColor,
+  borderColor,
+  fullWidth,
+}) => {
+  const buttonBackgroundColor = backgroundColor || 'bg-coral-red';
+  const buttonTextColor = textColor || 'text-white';
+  const buttonBorderColor = borderColor || 'border-coral-red';
+  const buttonFullWidth = fullWidth ? 'w-full' : '';
+
   return (
-    <button className="flex justify-center items-center 
-            gap-2 px-8 py-4 font-montserrat text-lg 
-            leading-none bg-coral-red rounded-full border-coral-red">
-        {label}
-        {iconURL && <img src={iconURL} alt="arrow right icon"
-        className="ml-2 rounded-full w-5 h-5"
-        />}
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full
+       ${buttonFullWidth} ${buttonBackgroundColor} ${buttonTextColor} ${buttonBorderColor}`}
+    >
+      {label}
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt='arrow right icon'
+          className='ml-2 rounded-full bg-white w-5 h-5'
+        />
+      )}
     </button>
-  )
-}
+  );
+};
 
 export default Button;
