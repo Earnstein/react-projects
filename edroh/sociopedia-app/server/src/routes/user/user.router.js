f70c61b61f7d324a4f9fb017d7076e7c14bcf8e2
Bakare Damilola <bdamilola00@gmail.com>
2023-09-25 20:32:27 +0100

    added Read and update logic in user controller

--- edroh/sociopedia-app/server/src/routes/user/user.router.js
+++ edroh/sociopedia-app/server/src/routes/user/user.router.js
@@ -1,10 +1,21 @@
-import express  from "express";
-import { login } from "../auth/auth.controller.js";
+import express from "express";
+import { verifyToken } from "../../middleware/auth.js";
+import {
+  httpGetUser,
+  httpGetUserFriends,
+  httpAddRemoveFriend,
+} from "./user.controller.js";
 
 const userRouter = express.Router();
 
-userRouter.post("/login", login)
 
-export {
-    userRouter
-}
+/* READ */
+userRouter.get("/:id", verifyToken, httpGetUser);
+userRouter.get("/:id", verifyToken, httpGetUserFriends);
+
+
+/* UPDATE */
+userRouter.patch("/:id/:friendId", verifyToken, httpAddRemoveFriend);
+
+
+export { userRouter };
