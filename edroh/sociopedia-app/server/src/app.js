5887bae4bbe2dd176d3c8067acee815d7da3aea4
Bakare Damilola <bdamilola00@gmail.com>
2023-09-26 13:03:03 +0100

    added post route  for creating posts

--- edroh/sociopedia-app/server/src/app.js
+++ edroh/sociopedia-app/server/src/app.js
@@ -9,15 +9,18 @@ import { fileURLToPath } from "url";
 import { register } from "./routes/auth/auth.controller.js";
 import { userRouter } from "./routes/user/user.router.js";
 import { authRouter } from "./routes/auth/auth.router.js";
+import { postRouter } from "./routes/post/post.router.js";
+import { verifyToken } from "./middleware/auth.js";
+import { httpCreatePost } from "./routes/post/post.contoller.js";
 
 
+/* FILEPATH AND DIRECTORIES CONFIGURATION */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+/* CONFIGURATION / MIDDLEWARES SETUP*/
 const app = express();
 
-
-/* CONFIGURATION / MIDDLEWARES SETUP*/
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
@@ -40,11 +43,13 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/post", verifyToken, upload.single("picture"), httpCreatePost);
 
 
 
 /*APP ROUTES */
 app.use("/auth", authRouter);
 app.use("/user", userRouter);
+app.use("/post", postRouter);
 
 export default app;
