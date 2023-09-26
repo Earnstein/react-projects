5d6e712b6563aa76a39f7b2b7f5ecfd8e3b8689f
Bakare Damilola <bdamilola00@gmail.com>
2023-09-26 13:04:58 +0100

    renamed file for easy identification

--- edroh/sociopedia-app/server/src/routes/auth/auth.controller.js
+++ edroh/sociopedia-app/server/src/routes/auth/auth.controller.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-import User from "../../models/User.js";
+import User from "../../models/User.model.js";
 dotenv.config({ path: "server/.env" });
 
 /*REGISTER USER*/
