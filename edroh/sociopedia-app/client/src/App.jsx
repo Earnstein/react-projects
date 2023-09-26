ba3a60d4fd1df36e3b12b51f14102dd6e0d49d39
Bakare Damilola <bdamilola00@gmail.com>
2023-09-26 16:26:07 +0100

    set up client and redux

---
+++ edroh/sociopedia-app/client/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomePage, LoginPage, ProfilePage } from "./scenes";
+
+function App() {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ edroh/sociopedia-app/client/src/scenes/index.js
@@ -0,0 +1,10 @@
+import HomePage from "./homePage/HomePage";
+import LoginPage from "./loginPage/LoginPage";
+import ProfilePage from "./profilePage/ProfilePage";
+
+
+export {
+    HomePage,
+    LoginPage,
+    ProfilePage
+}
