b13b7aee9a9e388b6be867c43870179b34d0c2a5
Bakare Damilola <bdamilola00@gmail.com>
2023-11-04 19:25:01 +0100

    testing post-audio endpoint

--- eAI/server/main.py
+++ eAI/server/main.py
@@ -1,23 +1,19 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
-from functions.openai import convert_audio_to_text , get_chat_response
-from functions.database import save_msg_to_database, reset_messages
-from functions.text_to_speech import convert_text_to_speech
+from decouple  import config
+from classes.openai_requests import AudioConverter, Chats
+from classes.database import Model
+
+
 app = FastAPI()
 
-# CORS - Origins
+# CORS SETUP
 
 origins = [
     "http://localhost:5173",
-    "http://localhost:5174",
-    "http://localhost:8080",
-    "http://localhost:3000",
+    "http://localhost:3000"
 ]
-
-# CORS - Middlware
-
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -27,36 +23,35 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+# Routes
+
 @app.get("/")
 async def home():
-    return {"Hello": "Everyone", "World": "swagger"}
+    return{"message": "homepage"}
 
-@app.get("/reset")
-async def reset_conversation():
-    reset_messages()
-    return 
 
-@app.post("/post-audio/")
-async def getAudio(file: UploadFile = File(...)):
+@app.get("/get-audio")
+def get_audio():
+    # Initialise database
+    database = Model("data.json")
 
-    ai_audio_response = convert_audio_to_text(file)
-    if not ai_audio_response:
-        return HTTPException(status_code=400, detail="failed to decode audio")
-    
-    #Gpt response
-    chat_response = get_chat_response(ai_audio_response)
+    # This would be dynamic soon as mp3 file would be coming from frontend
+    whisper = AudioConverter("my-voice.mp3")
+    audio_input = whisper.convert_audio_to_text()
 
-    if not chat_response:
-        return HTTPException(status_code=400, detail="failed to get a response")
-    
-    save_msg_to_database(ai_audio_response, chat_response)
+    if not audio_input:
+        return HTTPException(status_code=400, detail="failed to get audio input")
 
-    audio_output = convert_text_to_speech(chat_response)
-    if not audio_output:
-        return HTTPException(status_code=400, detail="failed to generate code")
-    
-    def iterfile():
-        yield audio_output
+    chatgpt = Chats(audio_input)
 
-    return StreamingResponse(iterfile(), media_type="application/octet-stream")
+    chat_response_text = chatgpt.get_chat_response()
 
+    # save whisper text and chatgpt response to database
+
+    database.save_msg_to_database(audio_input, chat_response_text)
+    print(chat_response_text)
+
+# @app.post("/post-audio/")
+# def post_audio(file: UploadFile = File(...)):
+#     return{"message": "post endpoint"}
