b41bcd7dc4058c024fc57367cb8b2b6155921b85
Bakare Damilola <bdamilola00@gmail.com>
2023-11-05 17:03:31 +0100

    initial commit

---
+++ eGPT/client/src/App.tsx
@@ -0,0 +1,6 @@
+
+const App = () => {
+  return <main>hello World</main>;
+};
+
+export default App;
---
+++ eGPT/client/src/main.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["roboto Slab, serif"].join(","),
+    allVariants: {
+      color: "white",
+    },
+  },
+});
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
