1a8cbb5709e452647c2abf2efd5f0dba6124660e
Bakare Damilola <bdamilola00@gmail.com>
2024-01-01 21:11:35 +0100

    added hover styles to links

--- my-portfolio-website/src/components/Link.jsx
+++ my-portfolio-website/src/components/Link.jsx
@@ -1,17 +1,29 @@
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
-const Link = ({page, selectedPage, setSelectedPage}) => {
-    const lowerCasePage = page.toLowerCase();
-    const pageHandler = () => {setSelectedPage(lowerCasePage)}
-    return (
-      <AnchorLink 
-        className={`${selectedPage === lowerCasePage ? "text-yellow hover:text-white"  : ""}
-        hover:text-yellow transition duration-500 font-opensans`}
-        href={`#${lowerCasePage}`}
-        onClick={pageHandler}>
-        {page}
-        </AnchorLink>
-    )
+const Link = ({ page, selectedPage, setSelectedPage, isTopOfPage }) => {
+  const lowerCasePage = page.toLowerCase();
+  const pageHandler = () => {
+    setSelectedPage(lowerCasePage);
   };
+  return (
+    <AnchorLink
+      className={`${
+        selectedPage === lowerCasePage ? "text-yellow hover:text-white" : ""
+      } 
+        transition duration-500 font-opensans relative before:absolute
+        before:w-0 before:h-[2px] before:bg-blue before:top-full before:left-0
+        after:absolute
+        after:w-0 after:h-[2px] after:bg-red after:top-full after:right-0 
+        hover:before:w-1/2 before:translate-x-full  before:transition-all before:duration-500
+        hover:after:w-1/2 after:-translate-x-full  after:transition-all after:duration-500
+        ${isTopOfPage ? "" : "after:bg-yellow "}
+        `}
+      href={`#${lowerCasePage}`}
+      onClick={pageHandler}
+    >
+      {page}
+    </AnchorLink>
+  );
+};
 
 export default Link;
