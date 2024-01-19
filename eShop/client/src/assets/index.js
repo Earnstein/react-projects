cc1f6c4be27d4f73f64ad1b108188c75f01a8f7a
Bakare Damilola <bdamilola00@gmail.com>
2024-01-19 23:43:37 +0100

    initial commit

---
+++ eShop/client/src/assets/index.js
@@ -0,0 +1,18 @@
+import airPodImg from "./images/airpods.jpg";
+import alexaImg from "./images/alexa.jpg";
+import cameraImg from "./images/camera.jpg";
+import mouseImg from "./images/mouse.jpg";
+import phoneImg from "./images/phone.jpg";
+import playstationImg from "./images/playstation.jpg";
+import sampleImg from "./images/sample.jpg";
+
+
+export {
+    airPodImg,
+    alexaImg,
+    cameraImg,
+    mouseImg,
+    phoneImg,
+    playstationImg,
+    sampleImg
+}
