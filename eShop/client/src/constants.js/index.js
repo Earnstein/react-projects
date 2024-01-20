f81b494f7ab603f3f2d836728409a8cbde34e443
Bakare Damilola <bdamilola00@gmail.com>
2024-01-21 00:01:57 +0100

    project helper file

---
+++ eShop/client/src/constants.js/index.js
@@ -0,0 +1,14 @@
+export const Links = [
+  {
+    href: "/",
+    label: "Products",
+  },
+  {
+    href: "/categories",
+    label: "Categories",
+  },
+  {
+    href: "/Trending",
+    label: "Trending",
+  },
+];
