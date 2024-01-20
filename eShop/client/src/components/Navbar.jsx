693337ea954578132825586fb396507efe65b396
Bakare Damilola <bdamilola00@gmail.com>
2024-01-20 13:45:44 +0100

    shadcn layout

--- eShop/client/src/components/Navbar.jsx
+++ eShop/client/src/components/Navbar.jsx
@@ -1,19 +1,21 @@
 import Container from "@/components/Container";
 import { ModeToggle } from "@/components/mode-toggle";
-import { Button } from "@/components/ui/button";
+import { FaShopify, FaUser } from "react-icons/fa";
+import { Button } from "./ui/button";
 
 const Navbar = () => {
   return (
-    <header className=" flex items-center h-14 backdrop-blur-0 border-b">
+    <header className=" bg-teal-50 dark:bg-background flex items-center h-16 backdrop-blur-0 border-b ">
       <Container>
         <nav className="flex justify-between items-center">
-        <div>
-        <Button variant="link">
-          eShop
-        </Button>
-
-        </div>
-        <ModeToggle />
+          <div className="flex items-center gap-1">
+            <FaShopify className="w-10 h-10 rounded-full" />
+            <h1 className="text-xl font-playfair font-medium">eShop</h1>
+          </div>
+          <div className="hidden md:flex md:gap-4">
+            <Button size="sm" className="bg-blue-400 hover:bg-blue-500 px-5 py-2.5 text-sm font-medium font-montserrat"> <FaUser className="mr-2 text-white"/> Sign up</Button>
+            <ModeToggle />
+          </div>
         </nav>
       </Container>
     </header>
@@ -21,3 +23,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+// #23074d #E0EAFC
