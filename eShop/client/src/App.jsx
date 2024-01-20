0540da5deb76c610f0f8a23b312c34348eb3cff8
Bakare Damilola <bdamilola00@gmail.com>
2024-01-20 23:58:41 +0100

    (new) - router setup

--- eShop/client/src/App.jsx
+++ eShop/client/src/App.jsx
@@ -1,10 +1,16 @@
+import { Outlet } from "react-router-dom";
 import { ThemeProvider } from "@/components/theme-provider";
-import Navbar from "./components/Navbar";
+import Navbar from "@/components/Navbar";
+
+
 
 const App = () => {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-them">
+      <main>
       <Navbar/>
+      <Outlet/>
+      </main>
     </ThemeProvider>
   );
 };
