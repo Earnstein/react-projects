a771709558eed51b3f22f34130dd9775b98dedba
Bakare Damilola <bdamilola00@gmail.com>
2024-01-21 22:08:32 +0100

    refactor - modify max width on  container

--- eShop/client/src/components/Container.jsx
+++ eShop/client/src/components/Container.jsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils"
 
 const Container = ({children}) => {
   return (
-    <section className={cn("mx-auto max-w-7xl w-full")}>
+    <section className={cn("mx-auto max-w-screen-2xl w-full px-4 py-8 sm:px-6 sm:py-12 lg:px-8")}>
         {children}
     </section>
   )
