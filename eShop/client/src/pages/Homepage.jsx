3e4590c7534973ccf4036297d26476511de803c7
Bakare Damilola <bdamilola00@gmail.com>
2024-01-21 22:07:01 +0100

    refactor - change style

--- eShop/client/src/pages/Homepage.jsx
+++ eShop/client/src/pages/Homepage.jsx
@@ -4,12 +4,12 @@ import products from "@/assets/products";
 
 const Homepage = () => {
   return (
-    <section className="mt-20">
+    <section className="mt-12">
       <Container>
         <h1 className="font-palanquin dark:text-muted-foreground text-center scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
           Latest products.
         </h1>
-        <div className="max-w-7xl mx-auto py-4 px-8">
+        <div className="py-4 px-8">
           <div className="grid grid-cols-1 gap-4 mt-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
             {products.map((product) => (
               <ProductCard
