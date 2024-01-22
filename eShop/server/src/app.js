a365ea3047d2690df081cca216980d64a41892d3
Bakare Damilola <bdamilola00@gmail.com>
2024-01-22 14:14:05 +0100

    added cors

--- eShop/server/src/app.js
+++ eShop/server/src/app.js
@@ -1,11 +1,24 @@
 import express from 'express';
 import api from './routes/index.js';
+import morgan from "morgan"
+import cors from "cors"
 
-
-// Middlewares
 
 const app = express()
+// Middlewares
+
+app.use(cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
+}))
+
+app.use(express.json())
+app.use(morgan('dev'));
 
 
-app.use("/v3", api)
+
+
+app.use("/api/v3", api)
+
+
 export default app;
