9d07d1c6f54da772944453c492879f9edfc1a4d2
Bakare Damilola <bdamilola00@gmail.com>
2024-01-22 14:14:31 +0100

    added mongdb setup

--- eShop/server/src/server.js
+++ eShop/server/src/server.js
@@ -1,5 +1,6 @@
 import { config } from "dotenv"
 import app from "./app.js";
+import { connectToMongoDB } from "./utils/mongo.js";
 config()
 
 const PORT = process.env.PORT || 8000;
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 8000;
 const server = app;
 
 async function startServer(){
+    await connectToMongoDB();
     server.listen(PORT, () => {
         console.log(`server is listening on ${PORT}`)
     })
