78df429ed604c4c65627238f54ef400ff80dd68a
Bakare Damilola <bdamilola00@gmail.com>
2024-01-23 00:27:51 +0100

    added Order, product, review and user schema

---
+++ eShop/server/src/models/Review.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const Review = mongoose.model("Review", reviewSchema);
+export default Review;
