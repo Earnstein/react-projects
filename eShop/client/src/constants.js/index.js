02d71e09c1af7eeb50fde687a5e66b3c6717160d
Bakare Damilola <bdamilola00@gmail.com>
2024-01-24 20:28:29 +0100

    .

--- eShop/client/src/constants.js/index.js
+++ eShop/client/src/constants.js/index.js
@@ -1,3 +1,9 @@
+export const BASE_URL = process.env.NODE_ENV === "development" ? "http://localhost:5000/api/v3/" : ""
+export const PRODUCTS_URL = "/product";
+export const USERS_URL = "/user";
+export const ORDERS_URL = "/order";
+export const PAYPAL_URL = "/config/paypal";
+
 export const Links = [
   {
     href: "/",
