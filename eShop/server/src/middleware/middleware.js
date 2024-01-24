6782f991c8ae21589f6c2d6aff8309f067296063
Bakare Damilola <bdamilola00@gmail.com>
2024-01-24 20:41:08 +0100

    new - middleware

---
+++ eShop/server/src/middleware/middleware.js
@@ -0,0 +1,35 @@
+import { config } from "dotenv";
+import color from "colors";
+config();
+
+// Async wrapper for route handlers
+const asyncHandler = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next);
+};
+
+// Not found middleware
+const notFound = (req, res, next) => {
+  const error = new Error(`Not found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+// Error handling middleware
+const errorHandler = (err, req, res, next) => {
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message;
+
+  // Check for mongoose bad ObjectID error
+  if (err.name === "CastError" && err.kind === "ObjectId") {
+    message = `Resource not found`;
+    statusCode = 404;
+  }
+
+  res.status(statusCode).json({
+    message: message,
+    stack: process.env.NODE_ENV === "production" ? "🍩" : err.stack,
+  });
+  console.error(`${err}`.red.inverse);
+};
+
+export { errorHandler, asyncHandler, notFound };
