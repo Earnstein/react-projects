1146bdfe57217f75b5cdda4dd4f1dffb8bddb960
Bakare Damilola <bdamilola00@gmail.com>
2024-01-24 20:45:20 +0100

    added cors

--- eShop/server/src/server.js
+++ eShop/server/src/server.js
@@ -1,6 +1,7 @@
 import { config } from "dotenv"
 import app from "./app.js";
 import { connectToMongoDB } from "./utils/mongo.js";
+import { seedData, deleteData } from "./utils/seeder.js";
 config()
 
 const PORT = process.env.PORT || 8000;
@@ -10,6 +11,11 @@ const server = app;
 
 async function startServer(){
     await connectToMongoDB();
+    // data seeding 
+
+    
+    // await seedData();
+    // await deleteData();
     server.listen(PORT, () => {
         console.log(`server is listening on ${PORT}`)
     })
