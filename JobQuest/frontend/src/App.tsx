e5ae2e2f01d5437ed1e5479aa2815e2212c83c31
Bakare Damilola <bdamilola00@gmail.com>
2024-02-19 00:46:51 +0100

    feat: added routes

--- JobQuest/frontend/src/App.tsx
+++ JobQuest/frontend/src/App.tsx
@@ -1,11 +1,52 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  AdminPage,
+  DashboardPage,
+  ErrorPage,
+  HomePage,
+  LandingPage,
+  SignInPage,
+  SignUpPage,
+} from "@/pages/index.ts";
+import { ThemeProvider } from "./components/theme-provider";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <LandingPage />,
+      },
+      {
+        path: "admin",
+        element: <AdminPage />,
+      },
+      {
+        path: "dashboard",
+        element: <DashboardPage />,
+      },
+    ],
+  },
+  {
+    path: "signup",
+    element: <SignUpPage />,
+  },
+
+  {
+    path: "signin",
+    element: <SignInPage />,
+  },
+]);
 
 const App = () => {
   return (
-    <h1>
-      Hello world
-    </h1>
-  )
-}
+    <ThemeProvider defaultTheme="light" storageKey="theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
+};
 
-export default App
+export default App;
