547563c2e1f1795990daff5cefd60a053b07c3c2
Bakare Damilola <bdamilola00@gmail.com>
2024-02-27 23:54:42 +0100

    feat: removed redundant router

--- JobQuest/server/src/routes/index.ts
+++ JobQuest/server/src/routes/index.ts
@@ -1,11 +1,7 @@
 import { Hono } from "hono";
 import jobRouter from "./job";
-import userRouter from "./user";
 const api = new Hono();
 
-
-
-api.route("/user", userRouter);
 api.route("/job", jobRouter)
 
 export default api;
