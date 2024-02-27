1dfb15770892109230a4bd0e52a0d5c3f9cd2473
Bakare Damilola <bdamilola00@gmail.com>
2024-02-27 13:36:25 +0100

    chore: moved port to an env file

--- JobQuest/server/index.ts
+++ JobQuest/server/index.ts
@@ -1,9 +1,10 @@
 import app from "./src/app.ts";
 
-
-const PORT = 6002;
+const PORT = process.env.PORT || 6002;
 Bun.serve({
     fetch: app.fetch,
-    port: process.env.PORT || PORT,
+    port: PORT,
     hostname: "localhost"
-})
+});
+
+console.log(`Listening on port : ${PORT}`);
