d13230a39ef9885b9e8e04d71be3243680146b1a
Bakare Damilola <bdamilola00@gmail.com>
2024-02-27 23:52:55 +0100

    feat: Job creation and job retrival CRUD

--- JobQuest/server/src/controllers/job.ts
+++ JobQuest/server/src/controllers/job.ts
@@ -1,44 +1,75 @@
 import type { Context } from "hono";
-import { nanoid } from "nanoid";
 import { jobs } from "../constants/job";
+import Job from "../models/job";
 
+
+//CREATE NEW JOB
 async function httpCreateJob(c: Context) {
-  const body = await c.req.json();
+  const { position, company } = await c.req.json();
 
-  if (!body.company || !body.position) {
-    return c.json({ message: "Missing property" }, 401);
+  if (!company || !position) {
+    throw new Error("Missing property");
   }
-  const job = {
-    id: nanoid(),
-    ...body,
-  };
-  jobs.push(job);
+  const job = new Job({
+    position: position,
+    company: company,
+  });
+  await job.save();
+  const newJob = await Job.findById(
+    {
+      _id: job._id,
+    },
+    {
+      "_id": 0,
+      "__v": 0,
+      createdAt: 0,
+      updatedAt: 0,
+    }
+  );
   return c.json(
     {
       message: "created",
-      job: body,
+      job: newJob,
     },
     201
   );
 }
 
-async function httpGetAllJob(c:Context) {
-    return c.json({ jobs })
-}
-async function httpGetJob(c:Context) {
-     const id = c.req.param("id");
-    if (!id) {
-        return c.json({message: "no id"}, 401)
-    }
-    const job = jobs.find(f => f.id === id)
-    if (!job){
-        return c.json({message: "Job does not exist"}, 401)
-    }
-    return c.json({message: 'success', data: job})
+
+// GET ALL JOB
+async function httpGetAllJob(c: Context) {
+  const allJobs = await Job.find({},  {
+    "_id": 0,
+    "__v": 0,
+    createdAt: 0,
+    updatedAt: 0,
+  })
+  if (allJobs.length != 0){
+    return c.json({
+      message: "ok",
+      data: allJobs
+    }, 200)
+  }
+  return c.json({
+    message: "ok",
+    data: "Your job list is Empty"
+  }, 200);
 }
 
-export {
-    httpCreateJob,
-    httpGetAllJob,
-    httpGetJob
+
+// GET JOB BY ID
+
+
+async function httpGetJob(c: Context) {
+  const id = c.req.param("id");
+  if (!id) {
+    return c.json({ message: "no id" }, 401);
+  }
+  const job = jobs.find((f) => f.id === id);
+  if (!job) {
+    return c.json({ message: "Job does not exist" }, 401);
+  }
+  return c.json({ message: "success", data: job });
 }
+
+export { httpCreateJob, httpGetAllJob, httpGetJob };
