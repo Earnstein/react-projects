be813a1a16704b149c27724761bef9da6af2b6a5
Bakare Damilola <bdamilola00@gmail.com>
2024-02-27 13:32:40 +0100

    add: added padding to outlet component

--- JobQuest/frontend/src/pages/dashboard-page.tsx
+++ JobQuest/frontend/src/pages/dashboard-page.tsx
@@ -32,11 +32,15 @@ const DashboardPage = () => {
         <Sidebar/>
         <div className="flex-1">
           <Navbar/>
-          <div>
+          <div className="py-16">
             <Outlet />
           </div>
         </div>
       </main>
+      <div className="p-10 bg-pink-700  w-full h-16 border-b-[1px] border-b-primary-foreground dark:border-b-accent  border-border/40 bg-background/95">
+          Lore mus fugit quidem eveniet dignissimos impedit, deserunt quaerat!
+          Delectus nostrum eius cumque molestiae recusandae, velit distinctio?
+        </div>
     </div>
  </DashBoardContext.Provider>
   );
