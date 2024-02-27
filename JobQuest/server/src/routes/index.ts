4935745c160cc24cd51d102c14a6b979de2db146
Bakare Damilola <bdamilola00@gmail.com>
2024-02-27 23:55:57 +0100

    add: user router

--- JobQuest/server/src/routes/index.ts
+++ JobQuest/server/src/routes/index.ts
@@ -1,7 +1,10 @@
 import { Hono } from "hono";
 import jobRouter from "./job";
+import userRouter from "./user";
 const api = new Hono();
 
 api.route("/job", jobRouter)
+api.route("/user", userRouter)
+
 
 export default api;
