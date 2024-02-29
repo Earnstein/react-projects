9bb7143146d687b4a296e4d9a45adf46610c07d5
Bakare Damilola <bdamilola00@gmail.com>
2024-02-29 22:17:58 +0100

    add: validator for body

--- JobQuest/server/src/routes/job.ts
+++ JobQuest/server/src/routes/job.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
 import { httpCreateJob, httpDeleteAllJob, httpDeleteJob, httpEditAJob, httpEditJob, httpGetAllJob, httpGetJob } from "../controllers/job";
+import { zValidator } from "@hono/zod-validator";
+import { schema } from "../middleware";
 
 
 const jobRouter = new Hono();
 
-jobRouter.route("/").post(httpCreateJob).get(httpGetAllJob).delete(httpDeleteAllJob)
+jobRouter.route("/").post(zValidator('json', schema), httpCreateJob).get(httpGetAllJob).delete(httpDeleteAllJob)
 jobRouter.route("/:id").get(httpGetJob).patch(httpEditJob).put(httpEditAJob).delete(httpDeleteJob)
 
 
