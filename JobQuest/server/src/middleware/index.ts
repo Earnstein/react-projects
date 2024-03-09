762a727fa11e78886370782d3b74d59c14c4d312
Bakare Damilola <bdamilola00@gmail.com>
2024-03-09 18:59:26 +0100

    add: patch , put and create validations

--- JobQuest/server/src/middleware/index.ts
+++ JobQuest/server/src/middleware/index.ts
@@ -1,7 +1,22 @@
 import * as z from 'zod';
+import { jobStatus, jobType } from '../constants/types';
 
-export const schema = z.object({
-    position: z.string()
-    .min(1, "Username must be at least 3 character")
-    .max(50)
+
+export const JobSchema = z.object({
+    position: z.string().min(1, "Position is required").max(50),
+    company:  z.string().min(1, "Company is required").max(50),
+    jobStatus:  z.enum([jobStatus.interview, jobStatus.declined, jobStatus.pending]),
+    jobType:  z.enum([jobType.fullTime, jobType.internship, jobType.partTime]),
+    jobLocation: z.string().min(1, "Job location is required"),
+
 })
+
+export const patchSchema = z.object({
+    position: z.string().min(1, "Position is required").max(50).optional(),
+    company: z.string().min(1, "Company is required").max(50).optional(),
+    jobStatus: z.enum([jobStatus.interview, jobStatus.declined, jobStatus.pending]).optional(), 
+    jobType: z.enum([jobType.fullTime, jobType.internship, jobType.partTime]).optional(),
+    jobLocation: z.string().min(1, "Job location is required").optional(),
+}).refine(data => data.position || data.company || data.jobType || data.jobLocation || data.jobType, {
+    message: "At least one of position, company, jobStatus, jobType, or jobLocation must be provided"
+});
