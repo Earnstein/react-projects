73c0b7950de6420b7ba732f93783fcf4ad11c75b
Bakare Damilola <bdamilola00@gmail.com>
2024-03-09 19:00:17 +0100

    add: types  file

---
+++ JobQuest/server/src/constants/types.ts
@@ -0,0 +1,18 @@
+export enum jobStatus {
+  interview = "interview",
+  declined = "declined",
+  pending = "pending"
+}
+
+export enum jobType {
+  fullTime = "full-time",
+  partTime = "part-time",
+  internship = "internship"
+}
+
+export interface Job_Sort_By {
+    NEWEST: string,
+    OLDEST: string,
+    ASCENDING: string,
+    DESCENDING: string
+}
