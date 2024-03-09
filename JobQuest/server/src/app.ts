4dfa64babef6042f16d319fe085bc5e6aaea4264
Bakare Damilola <bdamilola00@gmail.com>
2024-03-09 20:07:35 +0100

    chore: refactor controllers

--- JobQuest/server/src/app.ts
+++ JobQuest/server/src/app.ts
@@ -5,46 +5,59 @@ import { prettyJSON } from "hono/pretty-json";
 import { secureHeaders } from "hono/secure-headers";
 import { config } from "dotenv";
 import api from "./routes";
-import { NotFound, Unauthenticated, Unauthorized, BadRequest } from './middleware/error';
+import {
+  NotFound,
+  Unauthenticated,
+  Unauthorized,
+  BadRequest,
+} from "./middleware/error";
 import { MongooseError } from "mongoose";
-config()
+config();
 
 const app = new Hono();
 
-app.use(prettyJSON({space: 4}))
-app.use(secureHeaders())
-app.use(cors())
-app.use(logger())
-app.route("/api/v1", api)
+app.use(prettyJSON({ space: 4 }));
+app.use(secureHeaders());
+app.use(cors());
+app.use(logger());
+app.route("/api/v1", api);
 
 app.notFound((c) => {
-    return c.json({
-        message: "Not found"
-    }, 404)
-})
+  return c.json(
+    {
+      message: "Not found",
+    },
+    404
+  );
+});
 
 app.onError((err, c) => {
-   if (err instanceof NotFound 
-    || err instanceof BadRequest
-    || err instanceof Unauthenticated
-    || err instanceof Unauthorized) {
+  if (err instanceof MongooseError && err.name === "CastError") {
+    throw new BadRequest("Invalid id");
+  }
+
+  if (
+    err instanceof NotFound ||
+    err instanceof BadRequest ||
+    err instanceof Unauthenticated ||
+    err instanceof Unauthorized
+  ) {
     const message = err.message;
-    const statusCode = err.statusCode
-    return c.json({
-        message: message
-    }, statusCode)
-   }
+    const statusCode = err.statusCode;
+    return c.json(
+      {
+        message: message,
+      },
+      statusCode
+    );
+  }
 
-   if( err instanceof MongooseError && err.name === "CastError"){
-    return c.json({
-        message: "Invalid Id",
-        id: c.req.param("id")
-    }, 404)
-   }
-    return c.json({
-        message: "Internal server error"
-    }, 500)
-})
-
+  return c.json(
+    {
+      message: "Internal server error",
+    },
+    500
+  );
+});
 
 export default app;
