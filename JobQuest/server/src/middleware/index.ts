a3aa6659735fb88e957668fe179b7224de1cf2b6
Bakare Damilola <bdamilola00@gmail.com>
2024-03-09 20:07:56 +0100

    add: remote value validation

--- JobQuest/server/src/middleware/index.ts
+++ JobQuest/server/src/middleware/index.ts
@@ -5,9 +5,9 @@ import { jobStatus, jobType } from '../constants/types';
 export const JobSchema = z.object({
     position: z.string().min(1, "Position is required").max(50),
     company:  z.string().min(1, "Company is required").max(50),
-    jobStatus:  z.enum([jobStatus.interview, jobStatus.declined, jobStatus.pending]),
-    jobType:  z.enum([jobType.fullTime, jobType.internship, jobType.partTime]),
-    jobLocation: z.string().min(1, "Job location is required"),
+    jobStatus:  z.enum([jobStatus.interview, jobStatus.declined, jobStatus.pending]).optional(),
+    jobType:  z.enum([jobType.fullTime, jobType.internship, jobType.partTime, jobType.remote]).optional(),
+    jobLocation: z.string().min(1, "Job location is required").optional(),
 
 })
 
@@ -15,8 +15,8 @@ export const patchSchema = z.object({
     position: z.string().min(1, "Position is required").max(50).optional(),
     company: z.string().min(1, "Company is required").max(50).optional(),
     jobStatus: z.enum([jobStatus.interview, jobStatus.declined, jobStatus.pending]).optional(), 
-    jobType: z.enum([jobType.fullTime, jobType.internship, jobType.partTime]).optional(),
+    jobType: z.enum([jobType.fullTime, jobType.internship, jobType.partTime, jobType.remote]).optional(),
     jobLocation: z.string().min(1, "Job location is required").optional(),
-}).refine(data => data.position || data.company || data.jobType || data.jobLocation || data.jobType, {
+}).refine(data => data.position || data.company || data.jobType || data.jobLocation || data.jobStatus, {
     message: "At least one of position, company, jobStatus, jobType, or jobLocation must be provided"
 });
