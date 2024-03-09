eb3656cfd4f2fbb2186993cdb7cde653f156bd8f
Bakare Damilola <bdamilola00@gmail.com>
2024-03-09 18:58:38 +0100

    add: patch , put and create validations

--- JobQuest/server/src/routes/job.ts
+++ JobQuest/server/src/routes/job.ts
@@ -1,13 +1,12 @@
 import { Hono } from "hono";
 import { httpCreateJob, httpDeleteAllJob, httpDeleteJob, httpEditAJob, httpEditJob, httpGetAllJob, httpGetJob } from "../controllers/job";
 import { zValidator } from "@hono/zod-validator";
-import { schema } from "../middleware";
+import { JobSchema, patchSchema } from "../middleware";
 
 
 const jobRouter = new Hono();
 
-jobRouter.route("/").post(zValidator('json', schema), httpCreateJob).get(httpGetAllJob).delete(httpDeleteAllJob)
-jobRouter.route("/:id").get(httpGetJob).patch(httpEditJob).put(httpEditAJob).delete(httpDeleteJob)
-
+jobRouter.route("/").post(zValidator("json", JobSchema), httpCreateJob).get(httpGetAllJob).delete(httpDeleteAllJob)
+jobRouter.route("/:id").get(httpGetJob).patch(zValidator("json", patchSchema), httpEditJob).put(zValidator("json", JobSchema), httpEditAJob).delete(httpDeleteJob)
 
 export default jobRouter;
