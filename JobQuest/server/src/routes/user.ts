7e5af567e760923bbfba46f7919283885e6eb048
Bakare Damilola <bdamilola00@gmail.com>
2024-03-10 12:48:52 +0100

    add: sign up and signin route

--- JobQuest/server/src/routes/user.ts
+++ JobQuest/server/src/routes/user.ts
@@ -1,7 +1,20 @@
 import { Hono } from "hono";
-
+import {
+  httpDeleteAllUsers,
+  httpGetAllUsers,
+  httpSignIn,
+  httpSignUp,
+} from "../controllers/user";
+import { zValidator } from "@hono/zod-validator";
+import { signUpSchema, signInSchema } from "../middleware";
 
 const userRouter = new Hono();
-userRouter.get("/", (c) => c.text("Welcome to user router"))
+userRouter
+  .route("/")
+  .post("/signup",zValidator("json", signUpSchema), httpSignUp)
+  .post("/signin", zValidator("json", signInSchema), httpSignIn)
+  .get("/users",httpGetAllUsers)
+  .delete("/delete", httpDeleteAllUsers)
+  
 
 export default userRouter;
