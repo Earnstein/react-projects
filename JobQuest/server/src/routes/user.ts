41faa84fde9e6ca7ed56f72a3a592fe8787abb15
Bakare Damilola <bdamilola00@gmail.com>
2024-03-11 23:19:36 +0100

    chore: added response for validation

--- JobQuest/server/src/routes/user.ts
+++ JobQuest/server/src/routes/user.ts
@@ -7,14 +7,30 @@ import {
 } from "../controllers/user";
 import { zValidator } from "@hono/zod-validator";
 import { signUpSchema, signInSchema } from "../middleware";
+import { StatusCodes } from "http-status-codes";
 
 const userRouter = new Hono();
 userRouter
   .route("/")
-  .post("/signup",zValidator("json", signUpSchema), httpSignUp)
-  .post("/signin", zValidator("json", signInSchema), httpSignIn)
-  .get("/users",httpGetAllUsers)
-  .delete("/delete", httpDeleteAllUsers)
-  
+  .post(
+    "/signup",
+    zValidator("json", signUpSchema, (result, c) => {
+      if (!result.success) {
+        return c.json({ error: result.error.issues }, StatusCodes.BAD_REQUEST);
+      }
+    }),
+    httpSignUp
+  )
+  .post(
+    "/signin",
+    zValidator("json", signInSchema, (result, c) => {
+      if (!result.success) {
+        return c.json({ error: result.error.issues }, StatusCodes.BAD_REQUEST);
+      }
+    }),
+    httpSignIn
+  )
+  .get("/users", httpGetAllUsers)
+  .delete("/delete", httpDeleteAllUsers);
 
 export default userRouter;
