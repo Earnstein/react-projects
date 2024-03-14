9931e68d7f052348fd6651a869459c222aa3759c
Bakare Damilola <bdamilola00@gmail.com>
2024-03-14 23:05:55 +0100

    feat: added authentication middleware

--- JobQuest/server/src/routes/index.ts
+++ JobQuest/server/src/routes/index.ts
@@ -1,8 +1,22 @@
-import { Hono } from "hono";
+import { Hono, type Env } from "hono";
 import jobRouter from "./job";
 import userRouter from "./user";
+import { getCookie } from 'hono/cookie'
+import { Unauthenticated } from "../middleware/error";
+import { verify } from "hono/jwt";
+import type { ObjectId } from "mongoose";
+
 const api = new Hono();
 
+api.use("/job/*",  async (c, next) => {
+   const cookie = getCookie(c, "auth_token")
+   if (!cookie){
+    throw new Unauthenticated("User not Authenticated")
+   }
+   const token = await verify(cookie,  Bun.env.JWT_SECRET!)
+   c.set("jwtPayload", token)
+   await next();
+  })
 api.route("/job", jobRouter)
 api.route("/auth", userRouter)
 
