76f923467db4152eb9ae2cdf0e6143c50b55f79c
Bakare Damilola <bdamilola00@gmail.com>
2024-03-16 11:02:19 +0100

    add: added validation middleware

--- JobQuest/server/src/routes/index.ts
+++ JobQuest/server/src/routes/index.ts
@@ -2,14 +2,14 @@ import { Hono } from "hono";
 import jobRouter from "./job";
 import userRouter from "./user";
 import authRouter from "./auth";
-import { authMiddleware } from "../middleware/auth";
+import { authMiddleware, validateAdmin } from "../middleware/auth";
 
 const api = new Hono();
 
 api.route("/auth", authRouter);
 
 api.use("/job/*", authMiddleware);
-api.use("/user/*", authMiddleware);
+api.use("/user/*", authMiddleware, validateAdmin);
 api.route("/job", jobRouter);
 api.route("/user", userRouter);
 
