5eec735e928ede5b18e2798da7a20193cf9ffc89
Bakare Damilola <bdamilola00@gmail.com>
2024-03-16 03:43:03 +0100

    chore: removed auth endpoint from user route

--- JobQuest/server/src/routes/user.ts
+++ JobQuest/server/src/routes/user.ts
@@ -1,36 +1,10 @@
 import { Hono } from "hono";
-import {
-  httpDeleteAllUsers,
-  httpGetAllUsers,
-  httpSignIn,
-  httpSignUp,
-} from "../controllers/user";
-import { zValidator } from "@hono/zod-validator";
-import { signUpSchema, signInSchema } from "../middleware";
-import { StatusCodes } from "http-status-codes";
+import { httpDeleteAllUsers, httpGetAllUsers } from "../controllers/user";
 
 const userRouter = new Hono();
 userRouter
   .route("/")
-  .post(
-    "/signup",
-    zValidator("json", signUpSchema, (result, c) => {
-      if (!result.success) {
-        return c.json({ error: result.error.issues }, StatusCodes.BAD_REQUEST);
-      }
-    }),
-    httpSignUp
-  )
-  .post(
-    "/signin",
-    zValidator("json", signInSchema, (result, c) => {
-      if (!result.success) {
-        return c.json({ error: result.error.issues }, StatusCodes.BAD_REQUEST);
-      }
-    }),
-    httpSignIn
-  )
-  .get("/users", httpGetAllUsers)
+  .get(httpGetAllUsers)
   .delete("/delete", httpDeleteAllUsers);
 
 export default userRouter;
