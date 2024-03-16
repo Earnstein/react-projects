fea9443e1e7dbb82851bdfb0ca7f08897e74646a
Bakare Damilola <bdamilola00@gmail.com>
2024-03-16 10:59:31 +0100

    add: all  function types

--- JobQuest/server/src/constants/types.ts
+++ JobQuest/server/src/constants/types.ts
@@ -1,3 +1,6 @@
+import type { ObjectId } from "mongoose";
+
+
 export enum jobStatus {
   interview = "interview",
   declined = "declined",
@@ -21,4 +24,48 @@ export interface Job_Sort_By {
     OLDEST: string,
     ASCENDING: string,
     DESCENDING: string
+}
+
+export interface Payload {
+  userId: ObjectId;
+  role: string;
+  exp: Number;
+  iat: Number;
+}
+
+export interface JobBody {
+  company: string;
+  position: string;
+  jobStatus?: string;
+  jobType?: string;
+  jobLocation?: string;
+}
+export interface Patch {
+  company?: string;
+  position?: string;
+  jobStatus?: string;
+  jobType?: string;
+  jobLocation?: string;
+}
+
+export interface Put {
+  company: string;
+  position: string;
+  jobStatus?: string;
+  jobType?: string;
+  jobLocation?: string;
+}
+
+
+export interface UserBody {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  location?: string;
+}
+
+export interface SignInType {
+  email: string;
+  password: string;
 }
