9ae0933a86734b409df953d9b8e60d3ade4846fa
Bakare Damilola <bdamilola00@gmail.com>
2024-03-16 11:01:27 +0100

    add: validate admin / added helper comments

--- JobQuest/server/src/middleware/auth.ts
+++ JobQuest/server/src/middleware/auth.ts
@@ -1,8 +1,15 @@
 import type { Context, Next } from "hono";
 import { getCookie } from "hono/cookie";
-import { BadRequest, NotFound, Unauthenticated, Unauthorized } from "../middleware/error";
 import { verify } from "hono/jwt";
 import Job from "../models/job";
+import {
+  BadRequest,
+  NotFound,
+  Unauthenticated,
+  Unauthorized,
+} from "../middleware/error";
+
+// VERIFY COOKIE MIDDLEWARE
 
 export const authMiddleware = async (c: Context, next: Next) => {
   const cookie = getCookie(c, "auth_token");
@@ -13,6 +20,8 @@ export const authMiddleware = async (c: Context, next: Next) => {
   c.set("jwtPayload", token);
   await next();
 };
+
+// VALIDATE OWNER MIDDLEWARE
 
 export const validateUser = async (c: Context, next: Next) => {
   const id = c.req.param("id");
@@ -26,8 +35,19 @@ export const validateUser = async (c: Context, next: Next) => {
   }
   const isAdmin = role === "admin";
   const isUser = job?.createdBy.toString() === userId;
-  if(!isAdmin && !isUser){
-    throw new Unauthorized("Permission denied")
+  if (!isAdmin && !isUser) {
+    throw new Unauthorized("Permission denied");
+  }
+  await next();
+};
+
+// VALIDATE ADMIN MIDDLEWARE
+
+export const validateAdmin = async (c: Context, next: Next) => {
+  const { role } = c.get("jwtPayload");
+  const isAdmin = role === "admin";
+  if (!isAdmin) {
+    throw new Unauthorized("Permission denied");
   }
   await next();
 };
