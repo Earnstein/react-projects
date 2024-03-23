b69a05544a42e8aa77d8ae921203ed3af2492ff4
earnstein <bdamilola00@gmail.com>
2024-03-23 21:07:21 +0100

    chore: removed auth controllers

--- JobQuest/server/src/controllers/user.ts
+++ JobQuest/server/src/controllers/user.ts
@@ -1,7 +1,4 @@
 import type { Context } from "hono";
-import { sign } from "hono/jwt";
-import { setCookie } from "hono/cookie";
-import type { Payload } from "../constants/types";
 import { StatusCodes } from "http-status-codes";
 import {
   createUser,
@@ -10,46 +7,6 @@ import {
   deleteAllUsers,
 } from "../mongo/user";
 
-
-// POST: CREATE NEW USER
-
-async function httpSignUp(c: Context) {
-  const body = await c.req.json();
-  const newUser = await createUser(body);
-  return c.json(
-    {
-      message: "created",
-      job: newUser,
-    },
-    StatusCodes.CREATED
-  );
-}
-
-// POST: SIGN IN USER
-
-async function httpSignIn(c: Context) {
-  const body = await c.req.json();
-  const existingUser = await findExistingUser(body);
-
-  const payload: Payload = {
-    userId: existingUser._id,
-    role: existingUser.role,
-    exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24,
-    iat: Math.floor(Date.now() / 1000),
-  };
-
-  const token = await sign(payload, Bun.env.JWT_SECRET!);
-  setCookie(c, "auth_token", token, {
-    httpOnly: true,
-    expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
-  });
-  return c.json(
-    {
-      message: "Success, user logged in",
-    },
-    StatusCodes.OK
-  );
-}
 
 //GET: FETCH ALL JOB
 
@@ -70,4 +27,4 @@ async function httpDeleteAllUsers(c: Context) {
   return c.json({ message: "deleted", data: user }, StatusCodes.OK);
 }
 
-export { httpSignUp, httpGetAllUsers, httpDeleteAllUsers, httpSignIn };
+export { httpGetAllUsers, httpDeleteAllUsers};
