9877237363725a31c38b7374fdce067afa81e9a7
earnstein <bdamilola00@gmail.com>
2024-03-23 21:06:53 +0100

    add: auth controller

---
+++ JobQuest/server/src/controllers/auth.ts
@@ -0,0 +1,62 @@
+import type { Context } from "hono";
+import { sign } from "hono/jwt";
+import { setCookie } from "hono/cookie";
+import type { Payload } from "../constants/types";
+import { StatusCodes } from "http-status-codes";
+import { createUser, findExistingUser } from "../mongo/user";
+
+// POST: CREATE NEW USER
+
+async function httpSignUp(c: Context) {
+  const body = await c.req.json();
+  const newUser = await createUser(body);
+  return c.json(
+    {
+      message: "created",
+      job: newUser,
+    },
+    StatusCodes.CREATED
+  );
+}
+
+// POST: SIGN IN USER
+
+async function httpSignIn(c: Context) {
+  const body = await c.req.json();
+  const existingUser = await findExistingUser(body);
+
+  const payload: Payload = {
+    userId: existingUser._id,
+    role: existingUser.role,
+    exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24,
+    iat: Math.floor(Date.now() / 1000),
+  };
+
+  const token = await sign(payload, Bun.env.JWT_SECRET!);
+  setCookie(c, "auth_token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
+  });
+  return c.json(
+    {
+      message: "Success, user logged in",
+    },
+    StatusCodes.OK
+  );
+}
+
+// GET: LOG OUT USER
+async function httpLogout(c: Context) {
+  setCookie(c, "auth_token", "", {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+  return c.json(
+    {
+      message: "Success, user logged out.",
+    },
+    StatusCodes.OK
+  );
+}
+
+export { httpSignUp, httpSignIn, httpLogout };
