cf29b888acc4163716052f1853b6ec15bc3cc8e6
earnstein <bdamilola00@gmail.com>
2024-03-27 20:03:36 +0100

    chore: remove validate admin

--- JobQuest/server/src/routes/index.ts
+++ JobQuest/server/src/routes/index.ts
@@ -2,14 +2,14 @@ import { Hono } from "hono";
 import jobRouter from "./job";
 import userRouter from "./user";
 import authRouter from "./auth";
-import { authMiddleware, validateAdmin } from "../middleware/auth";
+import { authMiddleware} from "../middleware/auth";
 
 const api = new Hono();
 
 api.route("/auth", authRouter);
 
 api.use("/job/*", authMiddleware);
-api.use("/user/*", authMiddleware, validateAdmin);
+api.use("/user/*", authMiddleware);
 api.route("/job", jobRouter);
 api.route("/user", userRouter);
 
