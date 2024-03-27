af2e8a83ee5010b5c73488718319fff7f948ed50
earnstein <bdamilola00@gmail.com>
2024-03-27 20:04:40 +0100

    add: edit user and current user route

--- JobQuest/server/src/routes/user.ts
+++ JobQuest/server/src/routes/user.ts
@@ -1,10 +1,28 @@
 import { Hono } from "hono";
-import { httpDeleteAllUsers, httpGetAllUsers } from "../controllers/user";
+import {
+  httpDeleteAllUsers,
+  httpEditUser,
+  httpGetAllUsers,
+  httpGetUser,
+} from "../controllers/user";
+import { zValidator } from "@hono/zod-validator";
+import { userPatchSchema } from "../middleware";
+import { StatusCodes } from "http-status-codes";
+import { validateAdmin } from "../middleware/auth";
 
 const userRouter = new Hono();
 userRouter
   .route("/")
-  .get(httpGetAllUsers)
-  .delete("/delete", httpDeleteAllUsers);
-
+  .get(validateAdmin, httpGetAllUsers)
+  .delete(validateAdmin, httpDeleteAllUsers)
+  .get("/current", httpGetUser)
+  .patch(
+    "/edituser",
+    zValidator("json", userPatchSchema, (result, c) => {
+      if (!result.success) {
+        return c.json({ error: result.error.issues }, StatusCodes.BAD_REQUEST);
+      }
+    }),
+    httpEditUser
+  );
 export default userRouter;
