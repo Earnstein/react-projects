fa2f87c3bb749484c882dea3e305f47d94886db5
earnstein <bdamilola00@gmail.com>
2024-03-27 20:01:52 +0100

    add: user patch schema

--- JobQuest/server/src/middleware/index.ts
+++ JobQuest/server/src/middleware/index.ts
@@ -10,7 +10,7 @@ export const JobSchema = z.object({
     jobLocation: z.string().min(1, "Job location is required").optional(),
 })
 
-export const patchSchema = z.object({
+export const jobPatchSchema = z.object({
     position: z.string().min(1, {message: "Position is required"}).max(50).optional(),
     company: z.string().min(1, {message:"Company is required"}).max(50).optional(),
     jobStatus: z.enum([jobStatus.interview, jobStatus.declined, jobStatus.pending]).optional(), 
@@ -29,6 +29,19 @@ export const signUpSchema = z.object({
     location: z.string().min(1, "location is required").max(50).optional(),
     role: z.enum([userRoleType.user, userRoleType.admin]).optional(),
 })
+
+export const userPatchSchema = z.object({
+    firstName: z.string().min(1, "firstName is required").max(50).optional(),
+    lastName:  z.string().min(1, "lastName is required").max(50).optional(),
+    email:  z.string().email("Invalid email").optional(),
+    password:  z.string().min(8, "password must be at least 8 character long").max(50).optional(),
+    location: z.string().min(1, "location is required").max(50).optional(),
+    role: z.enum([userRoleType.user, userRoleType.admin]).optional(),
+}).refine(data => data.firstName || data.lastName || data.email || data.password || data.location || data.role , {
+    message: "At least one of firstname, lastName, email, password, or location must be provided"
+})
+
+
 
 
 export const signInSchema = z.object({
