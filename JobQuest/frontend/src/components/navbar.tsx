8d576093860426c46c9e9adb9e02dabc2baae709
earnstein <bdamilola00@gmail.com>
2024-03-29 20:47:39 +0100

    chore: change context to zustand state

--- JobQuest/frontend/src/components/navbar.tsx
+++ JobQuest/frontend/src/components/navbar.tsx
@@ -2,17 +2,18 @@ import logo from "@/assets/images/logo.png";
 import { NavLink, useLocation } from "react-router-dom";
 import { Button } from "./ui/button";
 import { ModeToggle } from "./mode-toggle";
-import { useDashBoardContext } from "@/hooks/useDashboardContext";
 import { MenuIcon } from "lucide-react";
 import MobileNavbar from "./mobile-navbar";
 import LogOutButton from "./drop-down";
+import { useDashboard } from "@/hooks/useDashboard";
 
 const Navbar = () => {
   const location = useLocation();
   const isHome = location.pathname === "/";
-  const data = useDashBoardContext();
+  const { toggleSidebar } = useDashboard();
+
   return (
-    <div className="fixed w-full h-16 border-b-[1px] border-b-primary-foreground dark:border-b-accent right-0 top-0 z-50 border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <div className="sticky w-full h-16 border-b-[1px] border-b-primary-foreground dark:border-b-accent right-0 top-0 z-50 border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container w-full flex justify-between items-center h-full mx-auto">
         {isHome ? (
           <div className="flex items-center gap-x-2">
@@ -27,17 +28,21 @@ const Navbar = () => {
             <div className="flex items-center gap-x-4">
               <MobileNavbar />
               <div className="flex items-center gap-x-4">
-              <div className="grid place-content-start ml-8 py-[13.5px]">
-              <div className="flex gap-x-2">
-                <h4 className="hidden sm:inline-block font-playfair text-accent-foreground text-3xl tracking-widest">
-                  LandIt
-                </h4>
-              </div>
-            </div>
-                <MenuIcon
-                  className="hidden md:block w-8 h-8"
-                  onClick={data.toggleSidebar}
-                />
+                <div className="grid place-content-start py-[13.5px]">
+                  <div className="flex gap-x-2 items-center">
+                    <Button
+                    variant="ghost"
+                    className="hidden md:inline-flex items-center p-2 w-10 h-10 transition-all duration-100 rounded-full"
+                    >
+                      <MenuIcon
+                      onClick={toggleSidebar}
+                    />
+                    </Button>
+                    <h4 className="hidden sm:inline-block font-playfair text-accent-foreground text-xl tracking-widest">
+                      LandIt
+                    </h4>
+                  </div>
+                </div>
               </div>
             </div>
           </>
@@ -65,9 +70,9 @@ const Navbar = () => {
               </NavLink>
             </>
           ) : (
-            <LogOutButton/>
+            <LogOutButton />
           )}
-          
+
           <ModeToggle />
         </ul>
       </div>
