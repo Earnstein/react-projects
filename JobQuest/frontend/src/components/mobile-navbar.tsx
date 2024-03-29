5621ac5e2c6e2d15184361c426504fbbc4a065f4
earnstein <bdamilola00@gmail.com>
2024-03-29 20:46:54 +0100

    chore: refactored mobile nav

--- JobQuest/frontend/src/components/mobile-navbar.tsx
+++ JobQuest/frontend/src/components/mobile-navbar.tsx
@@ -5,35 +5,43 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import {  MenuIcon } from "lucide-react";
+import { MenuIcon } from "lucide-react";
 import useMediaQuery from "@/hooks/useMediaQuery";
 import NavLinks from "@/components/Navlinks";
-import { Separator } from "@/components//ui/separator";
+import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
 import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 const MobileNavbar = () => {
-  const [open, setOpen] = useState(false)
-  const isMobile = useMediaQuery("(min-width: 1060px)");
+  const [open, setOpen] = useState(false);
+  const isNotMobile = useMediaQuery("(min-width: 1060px)");
   return (
     <>
-      {!isMobile && (
+      {!isNotMobile && (
         <Sheet open={open} onOpenChange={setOpen}>
           <SheetTrigger asChild>
-            <MenuIcon className="md:hidden block w-8 h-8" />
+            <Button
+              variant="ghost"
+              className="md:hidden inline-flex items-center p-2 w-10 h-10 transition-all duration-100 rounded-full"
+            >
+              <MenuIcon />
+            </Button>
           </SheetTrigger>
-          <SheetContent side="left">
+          <SheetContent side="left" className="w-64 sm:w-80 md:w-[350px]">
             <SheetHeader>
-              <SheetTitle className="font-playfair text-accent-foreground text-3xl tracking-widest py-6 px-8">LandIt</SheetTitle>
+              <SheetTitle className="font-playfair text-accent-foreground text-xl tracking-widest text-start py-4 pl-4">
+                LandIt
+              </SheetTitle>
             </SheetHeader>
             <div
-            className={cn(
-              "relative flex flex-col min-w-full duration-300 ease-out"
-            )}
-          >
-            <Separator />
-            <NavLinks onOpenChange={setOpen} />
-          </div>
+              className={cn(
+                "relative flex flex-col min-w-full duration-300 ease-out"
+              )}
+            >
+              <Separator />
+              <NavLinks onOpenChange={setOpen} />
+            </div>
           </SheetContent>
         </Sheet>
       )}
