272f8ac7235edca024ba2505c4699346dda94b9f
earnstein <bdamilola00@gmail.com>
2024-03-30 12:38:46 +0100

    add: logout state

--- JobQuest/frontend/src/hooks/useDashboard.tsx
+++ JobQuest/frontend/src/hooks/useDashboard.tsx
@@ -1,16 +1,19 @@
 import { create } from "zustand"
 
+type func = () => void;
+
 interface useDashBoardStore{
     user: String;
     showSidebar: boolean;
     toggleSidebar: () => void;
-    logout: () => void;
+    logout: (funct: func) => void;
   }
   
  export const useDashboard  =  create<useDashBoardStore>(((set) => ({
     user: "",
     showSidebar: false,
     toggleSidebar: () => set((state) => ({ showSidebar: !state.showSidebar })),
-    logout: () => {
+    logout: (f) => {
+      return f()
     },
   })))
