1065f90a261f7a6967b0acae49df6eb1aab3a52d
earnstein <bdamilola00@gmail.com>
2024-03-30 12:38:10 +0100

    feat: sign up feature

--- JobQuest/frontend/src/components/sign-up-form.tsx
+++ JobQuest/frontend/src/components/sign-up-form.tsx
@@ -12,9 +12,13 @@ import {
 } from "./ui/form";
 import { Input } from "./ui/input";
 import { cn } from "@/lib/utils";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Button } from "./ui/button";
-import google from "@/assets/images/google.svg";
+import { useMutation } from "@tanstack/react-query";
+import { userSignup } from "@/lib/requests";
+import { SignUp } from "@/constants/types";
+import { toast } from "sonner";
+import { Loader2 } from "lucide-react";
 
 const initialValuesRegister = {
   firstName: "",
@@ -25,27 +29,35 @@ const initialValuesRegister = {
 };
 
 const SignupForm = () => {
+  const navigate = useNavigate();
+  const { mutate, isPending } = useMutation({
+    mutationKey: ["sign-up"],
+    mutationFn: (body: SignUp) => userSignup(body),
+    onSuccess: () => {
+      toast.success("success!");
+      navigate("/signin");
+    },
+    onError: (error: any) => {
+      toast.error(error?.response?.data?.message);
+    },
+  });
+
   const form = useForm<z.infer<typeof registerSchema>>({
     resolver: zodResolver(registerSchema),
     defaultValues: initialValuesRegister,
   });
 
-  const isLoading = form.formState.isSubmitting;
-
   const onSubmitHandler = async (values: z.infer<typeof registerSchema>) => {
-    try {
-      console.log(values);
-      form.reset();
-    } catch (error) {
-      console.error("Form submission", error);
-    }
+    const { confirmPassword, ...formValues } = values;
+    mutate(formValues);
+    form.reset();
   };
   return (
     <div>
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmitHandler)}
-          className={cn("grid grid-cols-6 gap-5")}
+          className={cn("grid grid-cols-6 gap-3")}
         >
           {/* FIRST NAME FIELD */}
           <div className="md:col-span-3 col-span-6">
@@ -60,7 +72,7 @@ const SignupForm = () => {
                       className={cn(
                         "focus:border-indigo-300 focus:border-1 shadow-sm placeholder:font-palanquin"
                       )}
-                      disabled={isLoading}
+                      disabled={isPending}
                       placeholder="First name"
                       {...field}
                     />
@@ -84,7 +96,7 @@ const SignupForm = () => {
                       className={cn(
                         "focus:border-indigo-300 focus:border-1 shadow-sm placeholder:font-palanquin"
                       )}
-                      disabled={isLoading}
+                      disabled={isPending}
                       placeholder="Last name"
                       {...field}
                     />
@@ -108,7 +120,7 @@ const SignupForm = () => {
                       className={cn(
                         "focus:border-indigo-300 focus:border-1 shadow-sm placeholder:font-palanquin"
                       )}
-                      disabled={isLoading}
+                      disabled={isPending}
                       placeholder="Email"
                       {...field}
                     />
@@ -132,7 +144,7 @@ const SignupForm = () => {
                       className={cn(
                         "focus:border-indigo-300 focus:border-1 shadow-sm placeholder:font-palanquin"
                       )}
-                      disabled={isLoading}
+                      disabled={isPending}
                       type="password"
                       placeholder="Password"
                       {...field}
@@ -159,7 +171,7 @@ const SignupForm = () => {
                       className={cn(
                         "focus:border-indigo-300 focus:border-1 shadow-sm placeholder:font-palanquin"
                       )}
-                      disabled={isLoading}
+                      disabled={isPending}
                       placeholder="Confirm password"
                       type="password"
                       {...field}
@@ -172,7 +184,7 @@ const SignupForm = () => {
           </div>
 
           <div className="col-span-6 font-montserrat space-y-4">
-            <p className="text-end text-gray-700 text-sm w-full">
+            <p className="text-end  text-sm w-full">
               Already signed up?{" "}
               <span className="underline font-medium text-blue-400">
                 <Link to="/signin">Log in</Link>
@@ -180,22 +192,19 @@ const SignupForm = () => {
             </p>
             <Button
               className={cn(
-                "w-full px-2 py-3 text-sm border shrink-0 rounded-sm font-medium transition-all duration-250 border-indigo-600 bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none active:text-indigo-500 btn"
+                "w-full px-2 py-3 text-sm border shrink-0 rounded-md font-medium text-white font-montserrat"
               )}
               type="submit"
+              disabled={isPending}
             >
-              Create your account
-            </Button>
-
-            <Button
-              className={cn(
-                "flex justify-center items-center gap-4 justify-self-center w-full bg-white text-blue-500 font-bold font-palanquin rounded focus:outline-none focus:shadow-outline hover:bg-white/20 shadow-md"
+              {isPending ? (
+                <>
+                please wait {" "}
+                  <Loader2 className="h-4 w-4 ms-2 animate-spin" />
+                </>
+              ) : (
+                <>Create your account </>
               )}
-            >
-              <img src={google} alt="googleImg" />
-              <span className="text-secondary-foreground font-semibold font-opensans">
-                Continue with Google
-              </span>
             </Button>
           </div>
         </form>
