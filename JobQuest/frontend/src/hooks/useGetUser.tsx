3619a9b22f978408e0b0c6622c0a23a696499294
earnstein <bdamilola00@gmail.com>
2024-03-30 12:38:59 +0100

    add: get user hook

---
+++ JobQuest/frontend/src/hooks/useGetUser.tsx
@@ -0,0 +1,10 @@
+import { getUser } from "@/lib/requests"
+import { useQuery } from "@tanstack/react-query"
+
+export const useGetUser = () => {
+    return useQuery({
+        queryKey: ["user"],
+        queryFn: () => getUser()
+})
+}
+
