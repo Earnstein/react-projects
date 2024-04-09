f37b3477701be36e52b0f3cb32dae4418f128837
earnstein <bdamilola00@gmail.com>
2024-04-09 19:45:31 +0100

    feat: added protection layer to route

--- JobQuest/frontend/src/pages/dashboard-page.tsx
+++ JobQuest/frontend/src/pages/dashboard-page.tsx
@@ -1,24 +1,31 @@
 import Navbar from "@/components/navbar";
 import Sidebar from "@/components/sidebar";
+import { useGetUser } from "@/hooks/useGetUser";
 import { cn } from "@/lib/utils";
-import { Outlet } from "react-router-dom";
-
+import { Outlet, Navigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const DashboardPage = () => {
+  const { isError, error } = useGetUser();
+  if (isError) {
+    toast.error(error?.response?.data?.message);
+    return <Navigate to="/" />;
+  }
+
   return (
- <>
-     <div className="w-full h-full">
-      <main className={cn("flex")}>
-        <Sidebar/>
-        <div className="flex-1">
-          <Navbar/>
-          <div>
-            <Outlet />
+    <>
+      <div className="w-full h-full">
+        <main className={cn("flex")}>
+          <Sidebar />
+          <div className="flex-1">
+            <Navbar />
+            <div>
+              <Outlet />
+            </div>
           </div>
-        </div>
-      </main>
-    </div>
- </>
+        </main>
+      </div>
+    </>
   );
 };
 
