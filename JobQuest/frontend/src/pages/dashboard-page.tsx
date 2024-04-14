bb4e4d2912ff50020fd79112df077ae620de1e64
earnstein <bdamilola00@gmail.com>
2024-04-14 13:32:26 +0100

    chore: protected route

--- JobQuest/frontend/src/pages/dashboard-page.tsx
+++ JobQuest/frontend/src/pages/dashboard-page.tsx
@@ -2,12 +2,13 @@ import Navbar from "@/components/navbar";
 import Sidebar from "@/components/sidebar";
 import { useGetUser } from "@/hooks/useGetUser";
 import { cn } from "@/lib/utils";
+import { AxiosError } from "axios";
 import { Outlet, Navigate } from "react-router-dom";
 import { toast } from "sonner";
 
 const DashboardPage = () => {
   const { isError, error } = useGetUser();
-  if (isError) {
+  if (isError && error instanceof AxiosError) {
     toast.error(error?.response?.data?.message);
     return <Navigate to="/" />;
   }
