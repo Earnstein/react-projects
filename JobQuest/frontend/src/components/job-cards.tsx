d21f2041578904d1cecc3cf0d08908d14ba01ab8
earnstein <bdamilola00@gmail.com>
2024-04-15 00:14:20 +0100

    chore: added edit and delete modal

--- JobQuest/frontend/src/components/job-cards.tsx
+++ JobQuest/frontend/src/components/job-cards.tsx
@@ -1,31 +1,20 @@
 import { interviewLogo, pendingLogo, successLogo } from "@/assets/images";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Separator } from "./ui/separator";
-import { useGetJobs } from "../hooks/useGetJobs";
+import { useGetJobs } from "@/hooks/useGetJobs";
 import JobLoader from "./JobLoader";
-import { Button } from "./ui/button";
-import { DeleteIcon, EditIcon } from "lucide-react";
 import { JobBody } from "@/constants/types";
 import { cn } from "@/lib/utils";
-import { useDeleteJob } from "@/hooks/useDeleteJob";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
+import DeleteModal from "@/components/delete-modal";
+import EditModal from "@/components/edit-modal";
+import { Briefcase, MapPin, PieChart } from "lucide-react";
 
 const JobCards = () => {
   const { data, isLoading } = useGetJobs();
-  const { mutate, isPending } = useDeleteJob();
+  
 
   return (
-    <div className="mt-12 grid sm:grid-cols-2 lg:grid-cols-3 gap-3">
+    <div className="mt-0 grid sm:grid-cols-2 lg:grid-cols-3 gap-3">
       {isLoading && <JobLoader />}
       {!isLoading && (
         <>
@@ -75,13 +64,17 @@ const JobCards = () => {
                 </div>
                 <Separator className="mt-1" />
 
-                <div className="mt-2 pb-6 rounded-b-[--card-border-radius]">
-                  <p className="text-gray-700 dark:text-gray-300">
-                    {job.jobStatus}
+                <div className="space-y-3 py-6 rounded-b-[--card-border-radius]">
+                  <p className="flex items-center font-palanquin capitalize text-gray-700 dark:text-gray-300">
+                   <PieChart className="h-4 w-4 mr-2"/> {job.jobStatus}
                   </p>
 
-                  <p className="text-gray-700 dark:text-gray-300">
-                    {job.jobType}
+                  <p className="flex items-center font-palanquin capitalize text-gray-700 dark:text-gray-300">
+                  <Briefcase className="h-4 w-4 mr-2"/>  {job.jobType}
+                  </p>
+
+                  <p className="flex items-center font-palanquin capitalize text-gray-700 dark:text-gray-300">
+                   <MapPin className="h-4 w-4 mr-2"/> {job.jobLocation}
                   </p>
                 </div>
 
@@ -94,43 +87,8 @@ const JobCards = () => {
                     })}
                   </p>
                   <div className="flex gap-2">
-                    <Button className="group text-xs rounded-xl disabled:border *:select-none [&>*:not(.sr-only)]:relative *:disabled:opacity-20 disabled:text-gray-950 disabled:border-gray-200 disabled:bg-gray-100 dark:disabled:border-gray-800/50 disabled:dark:bg-gray-900 dark:*:disabled:!text-white text-gray-950 bg-gray-100 hover:bg-gray-200/75 active:bg-gray-100 dark:text-white dark:bg-gray-500/10 dark:hover:bg-gray-500/15 dark:active:bg-gray-500/10 flex gap-1.5 items-center  h-8 px-3.5 justify-center">
-                      <span>Edit</span>
-                      <EditIcon className="size-[1em]" />
-                    </Button>
-
-                    <AlertDialog>
-                      <AlertDialogTrigger asChild>
-                        <Button
-                          className="group text-xs rounded-xl disabled:border *:select-none [&>*:not(.sr-only)]:relative *:disabled:opacity-20 disabled:text-gray-950 disabled:border-gray-200 disabled:bg-gray-100 dark:disabled:border-gray-800/50 disabled:dark:bg-gray-900 dark:*:disabled:!text-white text-gray-950 bg-gray-100 hover:bg-gray-200/75 active:bg-gray-100 dark:text-white dark:bg-gray-500/10 dark:hover:bg-gray-500/15 dark:active:bg-gray-500/10 flex gap-1.5 items-center  h-8 px-3.5 justify-center bg-gradient-to-l from-red-100 to-white dark:bg-none"
-                          disabled={isPending}
-                        >
-                          <span>delete</span>
-                          <DeleteIcon className="size-[1em]" />
-                        </Button>
-                      </AlertDialogTrigger>
-                      <AlertDialogContent>
-                        <AlertDialogHeader>
-                          <AlertDialogTitle>
-                            Are you absolutely sure?
-                          </AlertDialogTitle>
-                          <AlertDialogDescription>
-                            This action cannot be undone.
-                          </AlertDialogDescription>
-                        </AlertDialogHeader>
-                        <AlertDialogFooter>
-                          <AlertDialogCancel>Cancel</AlertDialogCancel>
-                          <AlertDialogAction
-                            onClick={() => {
-                              mutate(job._id);
-                            }}
-                            disabled={isPending}
-                          >
-                            Continue
-                          </AlertDialogAction>
-                        </AlertDialogFooter>
-                      </AlertDialogContent>
-                    </AlertDialog>
+                    <EditModal id={job._id}/>
+                    <DeleteModal id={job._id}/>
                   </div>
                 </div>
               </div>
