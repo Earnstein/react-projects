9e6b0c7908ca70ec6c1b13c1f00335a43cd4bf32
earnstein <bdamilola00@gmail.com>
2024-04-14 11:59:48 +0100

    add: index file for assest import

---
+++ JobQuest/frontend/src/assets/images/index.ts
@@ -0,0 +1,12 @@
+import pendingLogo from "@/assets/images/pending.png";
+import interviewLogo from "@/assets/images/interviewing.png";
+import successLogo from "@/assets/images/success.png";
+
+
+
+
+export {
+    pendingLogo,
+    interviewLogo,
+    successLogo
+}
