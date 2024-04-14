6a1b26687dbdb2cb94780357ca524731571cc4c9
earnstein <bdamilola00@gmail.com>
2024-04-14 13:32:15 +0100

    chore: add job card

--- JobQuest/frontend/src/pages/jobs-page.tsx
+++ JobQuest/frontend/src/pages/jobs-page.tsx
@@ -1,6 +1,8 @@
+import JobCards from "@/components/job-cards"
+
 const JobsPage = () => {
   return (
-    <div>JobsPage</div>
+    <div className="container mt-8 mx-auto py-6 sm:py-8 rounded-sm"><JobCards/></div>
   )
 }
 
