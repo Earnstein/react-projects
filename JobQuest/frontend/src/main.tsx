0ecb0570774d4bfeaf50a08e8232907486cb8bf5
earnstein <bdamilola00@gmail.com>
2024-04-14 11:59:21 +0100

    chore: removed comment

--- JobQuest/frontend/src/main.tsx
+++ JobQuest/frontend/src/main.tsx
@@ -7,7 +7,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ToastProvider from '@/components/toast-provider.tsx';
 
 
-// React query setup
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
