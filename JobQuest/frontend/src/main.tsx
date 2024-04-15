98b3049608e5204307be124eca9a4444e7407dc2
earnstein <bdamilola00@gmail.com>
2024-04-15 21:04:06 +0100

    chore: removed tanstack setup

--- JobQuest/frontend/src/main.tsx
+++ JobQuest/frontend/src/main.tsx
@@ -1,20 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import ToastProvider from '@/components/toast-provider.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
+import ToastProvider from "@/components/toast-provider.tsx";
 
-const queryClient = new QueryClient()
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ToastProvider/>
+    <ToastProvider />
     <App />
-    <ReactQueryDevtools initialIsOpen={false}/>
-    </QueryClientProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
