47cc210ce9b6a37b93e8d2d9ab6142807129e9b0
earnstein <bdamilola00@gmail.com>
2024-04-15 22:16:52 +0100

    chore:  refactor file

--- JobQuest/frontend/src/hooks/useDashboard.tsx
+++ JobQuest/frontend/src/hooks/useDashboard.tsx
@@ -1,19 +1,10 @@
 import { create } from "zustand"
-
-type func = () => void;
-
 interface useDashBoardStore{
-    user: String;
     showSidebar: boolean;
     toggleSidebar: () => void;
-    logout: (funct: func) => void;
   }
   
  export const useDashboard  =  create<useDashBoardStore>(((set) => ({
-    user: "",
     showSidebar: false,
-    toggleSidebar: () => set((state) => ({ showSidebar: !state.showSidebar })),
-    logout: (f) => {
-      return f()
-    },
+    toggleSidebar: () => set((state) => ({ showSidebar: !state.showSidebar }))
   })))
