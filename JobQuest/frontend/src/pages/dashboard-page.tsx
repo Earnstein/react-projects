432a0d7397a5352727dc82291d33bede1001c33b
earnstein <bdamilola00@gmail.com>
2024-04-15 21:13:29 +0100

    chore: added axios interceptor to protect route

--- JobQuest/frontend/src/pages/dashboard-page.tsx
+++ JobQuest/frontend/src/pages/dashboard-page.tsx
@@ -1,17 +1,47 @@
 import Navbar from "@/components/navbar";
 import Sidebar from "@/components/sidebar";
-import { useGetUser } from "@/hooks/useGetUser";
 import { cn } from "@/lib/utils";
-import { AxiosError } from "axios";
-import { Outlet, Navigate } from "react-router-dom";
+import { Outlet, redirect } from "react-router-dom";
 import { toast } from "sonner";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useLogOut } from "@/hooks/useLogOut";
+import { getUser } from "@/lib/requests";
+import { QueryClient } from "@tanstack/react-query";
+
+const currentUser = {
+  queryKey: ["user"],
+  queryFn: getUser,
+};
+
+export const loader = (queryClient: QueryClient) => async () => {
+  try {
+    const query = await queryClient.ensureQueryData(currentUser);
+    return query;
+  } catch (error) {
+    return redirect("/");
+  }
+};
 
 const DashboardPage = () => {
-  const { isError, error } = useGetUser();
-  if (isError && error instanceof AxiosError) {
-    toast.error(error?.response?.data?.message);
-    return <Navigate to="/" />;
-  }
+  const [authError, setAuthError] = useState(false);
+  const { mutate } = useLogOut();
+
+  axios.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (error.response?.status === 401) {
+        toast.error(error?.response?.data?.message);
+        setAuthError(true);
+      }
+      return Promise.reject(error);
+    }
+  );
+
+  useEffect(() => {
+    if (!authError) return;
+    mutate();
+  }, [authError]);
 
   return (
     <>
