98b07eea6d23a36c0817d27859444dcf0ae2fdaa
earnstein <bdamilola00@gmail.com>
2024-04-15 21:04:35 +0100

    add: empty logo

--- JobQuest/frontend/src/assets/images/index.ts
+++ JobQuest/frontend/src/assets/images/index.ts
@@ -1,12 +1,13 @@
 import pendingLogo from "@/assets/images/pending.png";
 import interviewLogo from "@/assets/images/interviewing.png";
 import successLogo from "@/assets/images/success.png";
-
+import emptyLogo from "@/assets/images/empty.jpg";
 
 
 
 export {
     pendingLogo,
     interviewLogo,
-    successLogo
+    successLogo,
+    emptyLogo
 }
